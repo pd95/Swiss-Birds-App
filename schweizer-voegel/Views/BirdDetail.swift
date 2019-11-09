5921daabaaf04cf151400c22c313fe707497d65c
Philipp <pd95@users.noreply.github.com>
2019-11-10 00:55:40 +0100

    -Removing circle image -Make images "big" by adding them to ScrollView

--- schweizer-voegel/Views/BirdDetail.swift
+++ schweizer-voegel/Views/BirdDetail.swift
@@ -12,28 +12,17 @@ struct BirdDetail: View {
     var bird: Species
 
     var body: some View {
-        VStack {
-            HStack(alignment: .center) {
-                Image(bird.breadCrumbImageName)
+        ScrollView {
+            VStack {
+                Image(bird.primaryPictureName)
                     .resizable()
-                    .frame(width: 100, height: 100)
                     .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                    .shadow(radius: 3)
-                Text(bird.name)
-                    .font(.title)
-                    .fontWeight(.bold)
+                Image(bird.secondaryPictureName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
             }
-            Image(bird.primaryPictureName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Image(bird.secondaryPictureName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Spacer()
         }
-//        .navigationBarTitle(bird.name, displayMode: .inline)
+        .navigationBarTitle(bird.name)
     }
 }
 
