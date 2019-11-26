81ade9f8b13c7766392990947272500dec051690
Philipp <pd95@users.noreply.github.com>
2019-11-26 17:59:29 +0100

    Removing content of Birds.xcassets

---
+++ schweizer-voegel/Views/PlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerView.swift
+//  schweizer-voegel
+//
+//  Created by Philipp on 26.11.19.
+//  Copyright © 2019 Philipp. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
