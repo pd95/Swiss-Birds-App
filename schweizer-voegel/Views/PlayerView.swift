2c1ca955281c2693f1f0ac204d4eb9cfc167dc77
Philipp <pd95@users.noreply.github.com>
2019-11-27 20:51:05 +0100

    Addition of a PlayerView ... not yet used.

--- schweizer-voegel/Views/PlayerView.swift
+++ schweizer-voegel/Views/PlayerView.swift
@@ -7,15 +7,70 @@
 //
 
 import SwiftUI
+import AVKit
 
-struct PlayerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+class PlayerUIView: UIView {
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    // Override UIView property
+    override static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+    
+    
+    func play(url: URL) {
+        player = AVPlayer(url: url)
+        player?.play()
+    }
+
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//
+//        layer.addSublayer(playerLayer)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        playerLayer.frame = bounds
+//    }
+}
+
+struct PlayerView: UIViewRepresentable {
+    var assetName : String
+    var playerUIView : PlayerUIView? {
+        PlayerUIView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+        if playerUIView?.player == nil {
+            playerUIView?.play(url: URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!)
+        }
+    }
+    
+    func makeUIView(context: Context) -> UIView {
+        
+        return playerUIView!
     }
 }
 
 struct PlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        PlayerView()
+        PlayerView(assetName:"10.mp3")
     }
 }
