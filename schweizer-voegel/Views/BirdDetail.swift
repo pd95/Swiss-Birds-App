d070f277ae0da0fc774c0e7e296daafd96891187
Philipp <pd95@users.noreply.github.com>
2019-11-27 21:06:38 +0100

    Integrate bird voices in detail view

--- schweizer-voegel/Views/BirdDetail.swift
+++ schweizer-voegel/Views/BirdDetail.swift
@@ -7,26 +7,54 @@
 //
 
 import SwiftUI
+import AVKit
+
 
 struct BirdDetail: View {
     var bird: Species
+    
+    @State var isPlaying : Bool = false
 
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                Text(bird.alternateName)
-                    .font(.body)
+                HStack {
+                    Text(bird.alternateName)
+                        .font(.body)
+                    Spacer()
+                    Button(action: playVoice) {
+                        Text("Stimme: ")
+                        Image(systemName: isPlaying ? "stop.circle" : "play.circle")
+                    }
+                }
                 Image("assets/\(bird.primaryPictureName).jpg")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                Image("assets/\(bird.secondaryPictureName).jpg")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
+                if bird.secondaryPictureName.count > 0 {
+                    Image("assets/\(bird.secondaryPictureName).jpg")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
             }
             .padding()
         }
-        .navigationBarTitle(bird.name)
+        .navigationBarTitle(Text(bird.name), displayMode: .inline)
+        .onDisappear() {
+            stopSound()
+        }
     }
+    
+    func playVoice() {
+        isPlaying.toggle()
+        if isPlaying {
+            playSound(nameOfAudioFileInAssetCatalog: "assets/\(bird.speciesId).mp3")
+            isPlaying = alarmAudioPlayer?.isPlaying ?? false
+        }
+        else {
+            stopSound()
+        }
+    }
+    
 }
 
 struct BirdDetail_Previews: PreviewProvider {
@@ -37,3 +65,23 @@ struct BirdDetail_Previews: PreviewProvider {
         .environmentObject(ApplicationState())
     }
 }
+
+/// Audio player routines
+private var alarmAudioPlayer: AVAudioPlayer?
+func playSound(nameOfAudioFileInAssetCatalog: String) {
+    if let sound = NSDataAsset(name: nameOfAudioFileInAssetCatalog) {
+        do {
+            try! AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try! AVAudioSession.sharedInstance().setActive(true)
+            try alarmAudioPlayer = AVAudioPlayer(data: sound.data, fileTypeHint: AVFileType.mp3.rawValue)
+            alarmAudioPlayer!.play()
+        } catch {
+            print("error initializing AVAudioPlayer")
+        }
+    }
+}
+
+func stopSound() {
+    alarmAudioPlayer?.stop()
+}
+
