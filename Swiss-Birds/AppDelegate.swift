972dee54e59e4191b414f3f7d35fba78f19e13c7
Philipp <pd95@users.noreply.github.com>
2020-04-03 18:06:13 +0200

    Removing unused state variable

--- Swiss-Birds/AppDelegate.swift
+++ Swiss-Birds/AppDelegate.swift
@@ -12,9 +12,6 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var state = AppState()
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
