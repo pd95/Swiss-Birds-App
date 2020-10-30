c974c989cf0c1bdbccfa771a9cbc4a07861298ee
Philipp <pd95@users.noreply.github.com>
2020-10-30 14:53:25 +0100

    Updated scripts to build `Filter.xcassets` from downloaded SVGs

--- data-scripts/prepare_Filter.xcassets.sh
+++ data-scripts/prepare_Filter.xcassets.sh
@@ -13,24 +13,15 @@ if [ "x$CUSTOMIZE_SVG" == xtrue ]; then
   mkdir -p customized
   
   cd download/
-  for f in filter**.svg ; do 
+  for f in filter*.svg ; do 
       d="../customized/$f"
       echo "$f > $d"
-      xmllint "../download/$f" --xpath '//*[local-name()="svg"]/child::*[local-name()!="title" and local-name()!="desc"]'  \
-        | sed -e 's/fill="#FFFFFF"//g' \
-        -e '/<mask/d' \
-        -e '/<\/mask/d' \
-        -Ee 's/(fill-|stroke-)?opacity="[0-9.]+" *//g' \
-        -e 's/<path d="M1,10.9973333 C1,16.52 5.48022222,21 11.0028889,21 C16.5255556,21 21,16.52 21,10.9973333 C21,5.47466667 16.5255556,1 11.0028889,1 C5.48022222,1 1,5.47466667 1,10.9973333 Z" id="Path" stroke="#[^"]+" \/>//' \
-        -e 's/<path d="M1 10.997C1 16.52 5.48 21 11.003 21S21 16.52 21 10.997A9.995 9.995 0 0 0 11.003 1C5.48 1 1 5.475 1 10.997z" stroke="#000" \/>//' \
-        -e 's/<path class="st0" d="M1 11c0 5.5 4.5 10 10 10s10-4.5 10-10S16.5 1 11 1 1 5.5 1 11z"\/>//' \
-        -e 's/<path d="M11,.+" id="Ring"[^>]+>//g' \
-        -e '/<circle/d' \
+      xmllint "../download/$f" --xpath '//*[local-name()="svg"]/child::*[local-name()!="mask"]'  \
+        | sed \
+        -e 's/stroke-width="2"/stroke-width="1.5"/g' \
+        -e 's/<g mask[^>]+>//g' \
+        -e 's/clip-rule="evenodd" //g' \
         > "../temp.xml"
-
-      if [ "$f" == "filtervogelgruppe-67.svg" ]; then 
-        sed -i "" 's/a9490626802501180 9490626802501180/a0.1 0.1/' ../temp.xml
-      fi
 
       sed -e '/<!-- HERE IS YOUR PATH -->/ {' -e "r ../temp.xml" -e 'd' -e '}' "../../symbol-template.svg"  > "../customized/$f"
   done
