53b0491af1aaeee8d485d1721479066e5249a7cd
Philipp <pd95@users.noreply.github.com>
2021-11-16 20:04:07 +0100

    Start writing Unit Tests to enable refactoring later on. - Adding UnitTests testplan (and enabling it) - Adding a simple test for `AppState` verifying that initial load will finish eventually

--- Swiss-BirdsTests/Swiss_BirdsTests.swift
+++ Swiss-BirdsTests/Swiss_BirdsTests.swift
@@ -7,28 +7,25 @@
 //
 
 import XCTest
+import Combine
 @testable import Swiss_Birds
 
 class Swiss_BirdsTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_AppState_runsInitialLoad() {
+        let appState = AppState.shared
+        let exp = expectation(description: "Initial load should finish")
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        XCTAssertTrue(appState.initialLoadRunning, "Initial load should be running after creation.")
+        var cancellable: AnyCancellable?
+        cancellable = appState.objectWillChange
+            .sink { _ in
+                if appState.initialLoadRunning == false {
+                    exp.fulfill()
+                    cancellable?.cancel()
+                }
+            }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        wait(for: [exp], timeout: 10.0)  // We do not know how long it will take! :-(
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
