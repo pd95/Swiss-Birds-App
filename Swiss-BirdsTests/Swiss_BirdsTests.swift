8989a4f1a5504da6085876499979152f5de98012
Philipp <pd95@users.noreply.github.com>
2021-11-17 20:14:59 +0100

    Adding `SettingsStore` tests - validate defaults for a freshly initialised suite - ensure that setting `reset` to true will revert to the default values

--- Swiss-BirdsTests/Swiss_BirdsTests.swift
+++ Swiss-BirdsTests/Swiss_BirdsTests.swift
@@ -12,9 +12,48 @@ import Combine
 
 class Swiss_BirdsTests: XCTestCase {
 
+    // MARK: - Testing SettingsStore
+    func test_SettingsStore_validDefaults() {
+        let defaults = UserDefaults(suiteName: #function)!
+        let settings = SettingsStore(userDefaults: defaults)
+
+        XCTAssertEqual(settings.reset, false)
+        XCTAssertEqual(settings.startupCheckBirdOfTheDay, true)
+        XCTAssertEqual(settings.voiceDataOverConstrainedNetworkAccess, false)
+        XCTAssertEqual(settings.groupColumn, "")
+        XCTAssertEqual(settings.favoriteSpecies, [])
+
+        XCTAssertNotEqual(settings.appVersion, "-")
+    }
+
+    func test_SettingsStore_reset_changesBackToDefaults() {
+        let defaults = UserDefaults(suiteName: #function)!
+        let settings = SettingsStore(userDefaults: defaults)
+        let exp = expectation(description: #function)
+
+        settings.startupCheckBirdOfTheDay = false
+        settings.reset = true
+
+        var cancellable: AnyCancellable?
+        cancellable = defaults.publisher(for: \.reset)
+            .sink { value in
+                print("🟡 reset set to \(value)")
+                if value == false {
+                    print("🟡 Checking for default values")
+                    XCTAssertEqual(settings.startupCheckBirdOfTheDay, true)
+                    XCTAssertEqual(settings.reset, false)
+                    exp.fulfill()
+                    cancellable?.cancel()
+                }
+            }
+
+        wait(for: [exp], timeout: 5.0)  // We do not know how long it will take! :-(
+    }
+
+    // MARK: - Testing AppState
     func test_AppState_runsInitialLoad() {
         let appState = AppState()
-        let exp = expectation(description: "Initial load should finish")
+        let exp = expectation(description: #function)
 
         XCTAssertTrue(appState.initialLoadRunning, "Initial load should be running after creation.")
         var cancellable: AnyCancellable?
@@ -31,7 +70,7 @@ class Swiss_BirdsTests: XCTestCase {
 
     func test_AppState_loadsAllSpecies() {
         let appState = AppState()
-        let exp = expectation(description: "Initial load populates allSpecies property")
+        let exp = expectation(description: #function)
 
         XCTAssertEqual(appState.allSpecies.count, 0, "Initial species array must be empty.")
         var cancellable: AnyCancellable?
