709d1d8273fa811f41af9fbd1b352ad473162e4d
Philipp <pd95@users.noreply.github.com>
2023-03-06 18:34:06 +0100

    Create a new modern SwissBirds iOS App target, targeting iOS 16.2 and above.

---
+++ SwissBirds/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwissBirds
+//
+//  Created by Philipp on 06.03.23.
+//  Copyright © 2023 Philipp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwissBirds/SwissBirdsApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwissBirdsApp.swift
+//  SwissBirds
+//
+//  Created by Philipp on 06.03.23.
+//  Copyright © 2023 Philipp. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwissBirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
