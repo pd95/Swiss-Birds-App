bb90838981083224d7b64e67f69dc51e9d4451f0
Philipp <pd95@users.noreply.github.com>
2023-03-10 10:39:55 +0100

    Add "common" kind filters when loading and make sure updates are done on the `MainActor`.

--- SpeciesUI/SpeciesRepository.swift
+++ SpeciesUI/SpeciesRepository.swift
@@ -16,7 +16,7 @@ public class SpeciesRepository: ObservableObject {
         category: String(describing: SpeciesRepository.self)
     )
 
-    @Published public private(set) var species: [SpeciesCore.Species] = []
+    @Published public private(set) var species: [Species] = []
     @Published public private(set) var filters = FilterCollection()
 
     private let service: any DataService
@@ -31,7 +31,7 @@ public class SpeciesRepository: ObservableObject {
             async let species = self.service.fetchSpecies()
             async let filters = self.service.fetchFilters()
 
-            (self.species, self.filters) = try await (species, filters)
+            await update(try await (species, filters))
 
             Self.logger.log("fetch species done: \(self.species.count) fetched")
             Self.logger.log("fetch filters done: \(self.filters.allTypes.count) types fetched")
@@ -40,6 +40,14 @@ public class SpeciesRepository: ObservableObject {
             throw error
         }
     }
+
+    @MainActor
+    private func update(_ tuple: (species: [Species], filters: FilterCollection)) {
+        var tuple = tuple
+        tuple.filters.addFilter(type: "haeufigeart", id: 0, name: "rare kind")
+        tuple.filters.addFilter(type: "haeufigeart", id: 1, name: "common kind")
+        (self.species, self.filters) = tuple
+   }
 
     public func fetchDetails(for speciesID: Species.ID) async throws -> SpeciesDetail {
         Self.logger.log("fetching details for species with ID \(speciesID)")
